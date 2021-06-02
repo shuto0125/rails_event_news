80fcd5c539bb3bc23b736779f730dcbf566b779d
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2021-06-02 19:09:39 +0900

    ログアウト処理を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,8 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
   end
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
+  end
 end
