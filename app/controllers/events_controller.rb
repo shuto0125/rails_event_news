508786e77b88cce2c5539aefb69717b5dfac85f4
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2021-06-08 21:04:40 +0900

    保存したタグをshowに表示、[WIP]editでの表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @ticket = current_user && current_user.tickets.find_by(event: @event)
     @tickets = @event.tickets.includes(:user).order(:created_at)
+    # binding.pry
   end
   def new
     @event = current_user.created_events.build
